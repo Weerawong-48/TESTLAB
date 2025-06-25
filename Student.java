a53b875df0a067bdab25e09f85d4c5d1b7f86d68
PANANG6425 <100993424+PANANG6425@users.noreply.github.com>
2025-06-25 15:57:53 +0700

    4
    add constructor

--- Student.java
+++ Student.java
@@ -1,3 +1,17 @@
 class Student{
-  
-}
+  private int id;
+  private String name;
+
+  public Student(int id,String name){
+    this.id = id;
+    this.name = name;
+  }
+
+  public void PrintStudent(){
+    System.out.print("ID : "+this.id+" Name : "+this.name);
+  }
+};
+
+
+
+
